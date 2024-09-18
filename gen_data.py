9483cdd1c99b2677e6c7132891e585fff53166f0
Vuong Nguyen <vuong.se@gmail.com>
2024-09-18 11:38:51 -0400

    Add new supported data types

--- gen_data.py
+++ gen_data.py
@@ -1,10 +1,34 @@
 import csv
+from enum import Enum
 import random
 from faker import Faker
-
+import datetime as DT
 
 # from importlib import reload
 # from inspect import getsource
+
+
+class CsvDataType(Enum):
+    """
+    Enum of supported data types
+    """
+    NULL = 0
+    BOOL = 1
+    INT = 2
+    STRING = 3
+    FLOAT = 4
+    DATE = 5
+    DATETIME = 6
+
+
+MAX_INT_VALUE = 1000
+
+# 1 million
+MAX_FLOAT_VALUE = 1000_000.0
+
+# 10 years
+MAX_DATE_RANGE = 10*365
+
 
 def gen_data(nrows=10, ncols=5, titles=None, types=None):
     """
@@ -14,7 +38,7 @@ def gen_data(nrows=10, ncols=5, titles=None, types=None):
     if titles is not None and types is not None:
         assert len(titles) == len(types)
 
-    bools = [False, True]
+    bool_vals = [False, True]
 
     if titles is None:
         titles = [f'col_{i}' for i in range(1, ncols + 1)]
@@ -24,19 +48,30 @@ def gen_data(nrows=10, ncols=5, titles=None, types=None):
 
     rows = []
     fake = Faker()
+    today = DT.datetime.now()
+
     for _ in range(nrows):
 
         row = []
         for j in range(ncols):
             
-            if types[j] == 0:
+            if types[j] == CsvDataType.NULL:
                 col = None
-            if types[j] == 1:
-                col = random.randint(0, 1000)
-            elif types[j] == 2:
+            elif types[j] == CsvDataType.BOOL:
+                col = random.choice(bool_vals)
+            elif types[j] == CsvDataType.INT:
+                col = random.randint(0, MAX_INT_VALUE + 1)
+            elif types[j] == CsvDataType.STRING:
                 col = fake.name()
+            elif types[j] == CsvDataType.FLOAT:
+                col = random.random() * MAX_FLOAT_VALUE
+            elif types[j] == CsvDataType.DATE:
+                col = today.date() + DT.timedelta(days=-random.random() * MAX_DATE_RANGE)
+            elif types[j] == CsvDataType.DATETIME:
+                col = today + DT.timedelta(days=-random.random() * MAX_DATE_RANGE)
             else:
-                col = random.choice(bools)
+                raise Exception(f"Unsupported type: {types[j]}")
+            
             row.append(col)
             
         rows.append(row)
@@ -54,3 +89,22 @@ def write_csv(data, file):
         for row in data:
             wr.writerow(row)
 
+
+def run():
+
+    cols = [
+        *([CsvDataType.NULL]*1),
+        *([CsvDataType.BOOL]*1),
+        *([CsvDataType.INT]*3),
+        *([CsvDataType.STRING]*3),
+        *([CsvDataType.FLOAT]*3),
+        *([CsvDataType.DATE]*3),
+        *([CsvDataType.DATETIME]*3),
+    ]
+
+    rows = gen_data(10, len(cols), None, cols)
+
+    return rows
+
+
+run()
